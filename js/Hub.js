49033acbc0a81cb86ff82f3532f379d043fa1c60
Lokiheere <110852074+Lokiheere@users.noreply.github.com>
2023-04-10 19:24:23 -0400

    Update Hub.js

--- js/Hub.js
+++ js/Hub.js
@@ -61,7 +61,7 @@ let listProducts = [
     {
         id: 6,
         name: 'About Us',
-        image: 'img/Cardboard-small.png',
+        image: 'img/Cardboard-1 (1).png',
         link: 'AboutUs.html',
         nature: {
             type: 'About'
